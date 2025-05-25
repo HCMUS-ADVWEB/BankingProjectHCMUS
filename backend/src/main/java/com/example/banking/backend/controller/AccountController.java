07048fdf428e73bf71b7d3dc8ce67c38ff44410d
thinant <nguynthinan@outlook.com>
2025-05-25 23:41:00 +0700

    add services and endpoints
    customer get their account ep
    employee get customer's account ep
    
    there's no auth service so i use accountId path variable instead to test apis

--- backend/src/main/java/com/example/banking/backend/controller/AccountController.java
+++ backend/src/main/java/com/example/banking/backend/controller/AccountController.java
@@ -3,7 +3,12 @@ package com.example.banking.backend.controller;
 import com.example.banking.backend.dto.ApiResponse;
 import com.example.banking.backend.dto.request.account.CreateCustomerRequest;
 import com.example.banking.backend.dto.request.account.DepositRequest;
+import com.example.banking.backend.dto.response.account.GetAccountResponse;
+import com.example.banking.backend.dto.response.account.GetAccountTransactionsResponse;
+import com.example.banking.backend.model.type.TransactionType;
+import com.example.banking.backend.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,25 +19,37 @@ import java.util.UUID;
 @RequestMapping("/api/accounts")
 public class AccountController {
 
-    // private final AccountService accountService;
+    private final AccountService accountService;
 
-    @GetMapping
-    public ResponseEntity<ApiResponse<?>> getAccounts() {
-        return null;
+//    @GetMapping("/{accountId}")
+//    public ResponseEntity<ApiResponse<?>> getAccountDetails(@PathVariable UUID accountId) {
+//        return null;
+//    }
+
+//    @PostMapping("/{accountId}/deposit")
+//    public ResponseEntity<ApiResponse<Void>> deposit(@PathVariable UUID accountId, @RequestBody DepositRequest request) {
+//        return null;
+//    }
+
+//    @PostMapping("/create")
+//    public ResponseEntity<ApiResponse<?>> createCustomerAccount(@RequestBody CreateCustomerRequest request) {
+//        return null;
+//    }
+
+    @GetMapping("/customer/{accountId}")
+    public ResponseEntity<ApiResponse<GetAccountResponse>> getAccount(@PathVariable UUID accountId) {
+        ApiResponse<GetAccountResponse> apiResponse = accountService.getAccount(accountId);
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
 
-    @GetMapping("/{accountId}")
-    public ResponseEntity<ApiResponse<?>> getAccountDetails(@PathVariable UUID accountId) {
-        return null;
-    }
-
-    @PostMapping("/{accountId}/deposit")
-    public ResponseEntity<ApiResponse<Void>> deposit(@PathVariable UUID accountId, @RequestBody DepositRequest request) {
-        return null;
-    }
-
-    @PostMapping("/create")
-    public ResponseEntity<ApiResponse<?>> createCustomerAccount(@RequestBody CreateCustomerRequest request) {
-        return null;
+    @GetMapping("/employee/{accountId}")
+    public ResponseEntity<ApiResponse<GetAccountTransactionsResponse>> getAccountTransactions(
+            @PathVariable UUID accountId,
+            @RequestParam(required = false, defaultValue = "10") Integer limit,
+            @RequestParam(required = false, defaultValue = "0") Integer pn,
+            @RequestParam(required = false) TransactionType type
+    ) {
+        ApiResponse<GetAccountTransactionsResponse> apiResponse = accountService.getAccountTransactions(accountId, limit, pn, type);
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
 }
--- backend/src/main/java/com/example/banking/backend/service/AccountServiceImpl.java
+++ backend/src/main/java/com/example/banking/backend/service/AccountServiceImpl.java
@@ -6,19 +6,62 @@ import com.example.banking.backend.dto.request.account.RechargeAccountRequest;
 import com.example.banking.backend.dto.response.account.CreateCustomerAccountResponse;
 import com.example.banking.backend.dto.response.account.GetAccountResponse;
 import com.example.banking.backend.dto.response.account.GetAccountTransactionsResponse;
+import com.example.banking.backend.mapper.account.AccountMapper;
+import com.example.banking.backend.model.Account;
 import com.example.banking.backend.model.type.TransactionType;
+import com.example.banking.backend.repository.account.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class AccountServiceImpl implements AccountService {
+
+    private final AccountRepository accountRepository;
+
     @Override
     public ApiResponse<GetAccountResponse> getAccount(UUID accountId) {
-        return null;
+        Account account = accountRepository.findByAccountId(accountId);
+
+        if (account == null) {
+            return ApiResponse.<GetAccountResponse>builder()
+                    .status(HttpStatus.NO_CONTENT.value())
+                    .message("Account not found!")
+                    .build();
+        }
+
+        GetAccountResponse accountResponse = AccountMapper.INSTANCE.accountToGetAccountResponse(account);
+
+        return ApiResponse.<GetAccountResponse>builder()
+                .data(accountResponse)
+                .status(HttpStatus.OK.value())
+                .message("Account found successfully!")
+                .build();
     }
 
     @Override
     public ApiResponse<GetAccountTransactionsResponse> getAccountTransactions(UUID accountId, Integer size, Integer pagination, TransactionType type) {
-        return null;
+        Account account = accountRepository.getPaginatedTransactions(accountId, size, pagination, type);
+
+        if (account == null) {
+            return ApiResponse.<GetAccountTransactionsResponse>builder()
+                    .status(HttpStatus.NO_CONTENT.value())
+                    .message("Account not found!")
+                    .build();
+        }
+
+        GetAccountTransactionsResponse accountTransactionsResponse = AccountMapper.INSTANCE.accountToGetAccountTransactionsResponse(account);
+
+        return ApiResponse.<GetAccountTransactionsResponse>builder()
+                .data(accountTransactionsResponse)
+                .status(HttpStatus.OK.value())
+                .message("Account's transaction history found successfully!")
+                .build();
     }
 
     @Override
