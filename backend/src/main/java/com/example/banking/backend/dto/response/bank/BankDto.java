fdfda13ee22ac75d9e493a415407ad5acbb665bc
thinant <nguynthinan@outlook.com>
2025-06-24 17:00:04 +0700

    Bank specification

--- backend/src/main/java/com/example/banking/backend/dto/response/bank/BankDto.java
+++ backend/src/main/java/com/example/banking/backend/dto/response/bank/BankDto.java
@@ -1,14 +1,23 @@
 package com.example.banking.backend.dto.response.bank;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Data
+@Schema(description = "Bank's information")
 public class BankDto {
+    @Schema(description = "Bank's code",
+            example = "FIN")
     String bankCode ;
+
+    @Schema(description = "Bank's name",
+            example = "Fintech Hub")
     String bankName ;
 }
