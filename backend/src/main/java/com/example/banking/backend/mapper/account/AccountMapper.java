e4f62acdc59a2f951f1b51d2c6485b6669340ae6
21120372 <nguyenhungyen0000@gmail.com>
2025-06-26 21:57:17 +0700

    fix mapper

--- backend/src/main/java/com/example/banking/backend/mapper/account/AccountMapper.java
+++ backend/src/main/java/com/example/banking/backend/mapper/account/AccountMapper.java
@@ -2,8 +2,12 @@ package com.example.banking.backend.mapper.account;
 
 import com.example.banking.backend.dto.response.account.GetAccountResponse;
 import com.example.banking.backend.dto.response.account.GetAccountTransactionsResponse;
+import com.example.banking.backend.dto.response.transaction.TransactionInfoDto;
 import com.example.banking.backend.model.Account;
+import com.example.banking.backend.model.Bank;
+import com.example.banking.backend.model.Transaction;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
@@ -14,4 +18,17 @@ public interface AccountMapper {
     GetAccountResponse accountToGetAccountResponse(Account account);
 
     GetAccountTransactionsResponse accountToGetAccountTransactionsResponse(Account account);
+    @Mapping(target = "fromBankId", expression = "java(getBankId(transaction.getFromBank()))")
+    @Mapping(target = "toBankId", expression = "java(getBankId(transaction.getToBank()))")
+    TransactionInfoDto transactionToTransactionInfoDto(Transaction transaction);
+
+    default String getBankId(Bank bank) {
+        if (bank == null) return null;
+        try {
+            return bank.getId().toString();
+        } catch (Exception e) {
+            // Xử lý trường hợp lazy loading exception
+            return null;
+        }
+    }
 }
